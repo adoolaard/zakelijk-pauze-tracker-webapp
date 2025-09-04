060b895f8408c588673887a881f02de48c1c54c4
Abel <75690975+adoolaard@users.noreply.github.com>
2025-09-04 15:13:54 +0200

    Add break rule management and sidebar navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,25 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
+    <body class="font-sans antialiased" x-data="{ open: false }">
+        <div class="min-h-screen bg-gray-100 flex">
+            <div :class="{'block': open, 'hidden': !open}" class="fixed inset-0 bg-black bg-opacity-25 sm:hidden" @click="open=false"></div>
+            <nav :class="{'-translate-x-full': !open}" class="fixed z-30 w-64 bg-white h-full border-r transform transition-transform duration-150 ease-in-out sm:translate-x-0">
+                @include('layouts.navigation')
+            </nav>
+            <div class="flex-1 flex flex-col sm:ml-64">
+                <header class="bg-white shadow flex items-center justify-between p-4 sm:justify-end">
+                    <button @click="open = !open" class="sm:hidden text-gray-700 focus:outline-none">
+                        <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
+                    </button>
+                    @isset($header)
+                        <div class="text-gray-800 text-lg">{{ $header }}</div>
+                    @endisset
                 </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <main class="p-4">
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
     </body>
 </html>
